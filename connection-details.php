5f4d2aec6ab0535fe0f8b6037cff916975d67fce
marina-espinoza <35943671+marina-espinoza@users.noreply.github.com>
2018-06-07 12:16:05 -0400

    Relocated comments

--- connection-details.php
+++ connection-details.php
@@ -1,7 +1,7 @@
 <?php
 
 if(isset($_POST['connection_detail'])){
-    $connectionId = $_POST["connectionid"];
+	$connectionId = $_POST["connectionid"];
     // echo "connectionid = " . $connectionId;
 	connections_details($connectionId);
 }
@@ -11,16 +11,15 @@ function connections_details($connectionId) {
 	global $wpdb;
     $table_name = "Connections";
 
-    $myFeedbacks = $wpdb->get_results("SELECT *, users.`firstname` AS `sender`, users2.`firstname` AS `receiver` FROM `feedbacks` INNER JOIN users on feedbacks.SenderId=users.ID INNER JOIN users users2 on feedbacks.ReceiverId=users2.ID WHERE `connectionId` = $connectionId");
+    $myFeedbacks = $wpdb->get_results("SELECT *, Users.`firstname` AS `sender`, Users2.`firstname` AS `receiver` FROM `Feedbacks` INNER JOIN Users on Feedbacks.SenderId=Users.ID INNER JOIN Users Users2 on Feedbacks.ReceiverId=Users2.ID WHERE `connectionId` = $connectionId");
 
 	?>
-
-		<div class="list-group">
+		<div id="comments_display" name="<?php echo $connectionId; ?>" style="display:none" class="list-group">
 			<?php foreach ($myFeedbacks as $feedback) { ?>
  			<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
 		    	<div class="d-flex w-100 justify-content-between">
 			      <h5 class="mb-1">Sender: <?php echo $feedback->sender; ?></h5>
-			      <!-- <small>Rating : <?php echo $feedback->Rating; ?></small> -->
+			      <!--<small>Rating : <?php echo $feedback->Rating; ?></small>-->
 			    </div>
 			    <p class="mb-1">Comment: <?php echo $feedback->Description; ?></p>
 			    <small>To:  <?php echo $feedback->receiver; ?></small>
@@ -28,6 +27,8 @@ function connections_details($connectionId) {
 		  	</a>
 		<?php } ?>
 		</div>
-
 <?php
+
 }
+
+
