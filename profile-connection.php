80f5476f1ba62b751828bd8ee0098de42fe46820
marina-espinoza <35943671+marina-espinoza@users.noreply.github.com>
2018-08-03 14:53:03 -0400

    Change columns depending on user type

--- profile-connection.php
+++ profile-connection.php
@@ -6,79 +6,115 @@ function profile_connection() {
 	global $wpdb;
 
         if(isset($_SESSION['id'])){
-            $profileId = $_SESSION['id'];
+						$profileId = $_SESSION['id'];
             ?>
             <div class="alert alert-info">
   				<strong>Welcome</strong> <?php echo $_SESSION['login_user']?>
 			</div>
 			<?php
-	        $rows = $wpdb->get_results("SELECT `connectionid`, users.`firstname` AS `mentorName`, users2.`firstname` AS `menteeName`, `createdDate`, `lastConnected`,`menteeId`,`mentorId` FROM connections INNER JOIN users on connections.mentorId=users.ID INNER JOIN users users2 on connections.menteeId=users2.ID WHERE `mentorId` = $profileId OR `menteeId` = $profileId ");
-
-	        $myFeedbacks = $wpdb->get_results("SELECT *, users.`firstname` as `sender`, users2.`firstname` AS `receiver` FROM `feedbacks` INNER JOIN users on feedbacks.SenderId=users.ID INNER JOIN users users2 on feedbacks.ReceiverId=users2.ID WHERE `ReceiverId` = $profileId");
-	        $TCFeedBacks = $wpdb->get_results("SELECT * FROM `tc_feedbacks` WHERE `profileId` = $profileId");
-
-	        $userType = '0';
-	        if($_SESSION['userType'] == '1') {
-	        	$userType = '1';
-	        }
-
-	       
+					if($_SESSION['userType'] == '1') {
+						$rows = $wpdb->get_results("SELECT `connectionid`, `createdDate`, `lastConnected`, `menteeId`, `mentorId`,Users2.`firstname` AS `firstname`, Users2.`lastname` AS `lastname`, Users2.`email` AS `email`, Users2.`phonenumber` AS `phonenumber`, Users2.`resource` AS `resource`, Users2.`interest` AS `interest`, Users2.`tcAffiliation` AS `tcAffiliation` FROM Connections INNER JOIN Users on Connections.mentorId=Users.ID INNER JOIN Users Users2 on Connections.menteeId=Users2.ID WHERE `mentorId` = $profileId OR `menteeId` = $profileId ");
+						$userType = '1';
+					}
+					else{
+						$rows = $wpdb->get_results("SELECT `connectionid`, `createdDate`, `lastConnected`, `menteeId`, `mentorId`,Users.`firstname` AS `firstname`, Users.`lastname` AS `lastname`, Users.`email` AS `email`, Users.`phonenumber` AS `phonenumber`, Users.`jobTitle` AS `jobTitle`, Users.`companyName` AS `companyName` FROM Connections INNER JOIN Users on Connections.mentorId=Users.ID INNER JOIN Users Users2 on Connections.menteeId=Users2.ID WHERE `mentorId` = $profileId OR `menteeId` = $profileId ");
+						$userType = '0';
+					}
+					//$user_rows = $wpdb->get_results("SELECT `connectionid`, Users.`firstname` AS `mentorName`, Users2.`firstname` AS `menteeName`, `createdDate`, `lastConnected`,`menteeId`,`mentorId` FROM Connections INNER JOIN Users on Connections.mentorId=Users.ID INNER JOIN Users Users2 on Connections.menteeId=Users2.ID WHERE `mentorId` = $profileId OR `menteeId` = $profileId ");
+					$myFeedbacks = $wpdb->get_results("SELECT * FROM `Feedbacks` WHERE `ReceiverId` = $profileId");
+	        $TCFeedBacks = $wpdb->get_results("SELECT * FROM `TC_Feedbacks` WHERE `profileId` = $profileId");
 	?>
 
 		<div class="container">
 		  <table class="table table-striped">
 		    <thead>
 		      <tr>
-		        <th>Connection</th>
-		        <th>Last Checked In</th>
-		        <th>Feedback</th>
-		        <th>Email</th>
+						<?php
+						if ($userType == 0){ ?>
+								<th>First Name</th>
+								<th>Last name</th>
+								<th>Last Checked In</th>
+								<th>Phone Number</th>
+								<th>Job Title</th>
+								<th>Company</th>
+								<th>Feedback</th>
+								<th>Email</th>
+						<?php
+						}
+						else { ?>
+								<th>First Name</th>
+								<th>Last name</th>
+								<th>Last Checked In</th>
+								<th>Phone Number</th>
+								<th>Status</th>
+								<th>Interest</th>
+								<th>Relation</th>
+								<th>Feedback</th>
+								<th>Email</th>
+						<?php
+						}
+						?>
 		      </tr>
 		    </thead>
 		    <tbody>
 		      <?php foreach ($rows as $row) { ?>
 		      <tr>
-		        <td> 
-		          Connection Id:
-		          <a href="#"><?php echo $row->connectionid; ?></a><br>
-		          Mentor Name:
-		          <a href="../profile?user_id=<?php echo $row->mentorId;?>"><?php echo $row->mentorName; ?></a><br>
-		          Mentee Name:
-		          <a href="../profile?user_id=<?php echo $row->menteeId;?>"><?php echo $row->menteeName; ?></a><br>
-		        </td>
-		        <td> 
-		          Date Connected:
-		          <?php echo $row->createdDate; ?><br>
-		          Last Checked In:
-		          <?php echo $row->lastConnected; ?><br>
-		        </td>
-		        <td> 
+		        <td class="manage-column ss-list-width"><?php echo $row->firstname; ?></td>
+						<td class="manage-column ss-list-width"><?php echo $row->lastname; ?></td>
+						<td class="manage-column ss-list-width"><?php echo $row->lastConnected; ?></td>
+						<td class="manage-column ss-list-width"><?php echo $row->phonenumber; ?></td>
+						<?php
+						if ($userType == 0){
+							?>
+							<td class='manage-column ss-list-width'><?php echo $row->jobTitle; ?></td>
+							<td class='manage-column ss-list-width'><?php echo $row->companyName; ?></td>
+							<?php
+						}
+						else {
+							?>
+							<td class='manage-column ss-list-width'><?php echo $row->resource; ?></td>
+							<td class='manage-column ss-list-width'><?php echo $row->interest; ?></td>
+							<td class='manage-column ss-list-width'><?php echo $row->tcAffiliation; ?></td>
+							<?php
+						}
+						?>
+		        <td>
 		          <br>
 		          <form action="" method="post">
-		          	<input type="hidden" name="Rating" <?php echo "value=".$profileId;?>>
+		          	<span> Rating: </span>
+		          	<input type="radio" name="Rating" value="1"/>
+			        <label>1</label>
+			        <input type="radio" name="Rating" value="2"/>
+			        <label>2</label>
+			        <input type="radio" name="Rating" value="3"/>
+			        <label>3</label>
+			        <input type="radio" name="Rating" value="4"/>
+			        <label>4</label>
+			        <input type="radio" name="Rating" value="5"/>
+			        <label>5</label>
 			        <div class="form-group">
-					  <label for="comment">Please provide any comments or feedbacks to Tuesday's Children admin:</label>
+					  <label for="comment">Feedback:</label>
 					  <textarea class="form-control" rows="2" id="comment" name="Description"></textarea>
 					</div>
 		          	<button type="submit" class="btn btn-default" name="leave_feedback">
 		          	<input type="hidden" name="connectionid" <?php echo "value=".$row->connectionid;?>>
 		          	<input type="hidden" name="SenderId" <?php echo "value=".$profileId;?>>
-		          	<input type="hidden" name="ReceiverId" 
-		          	<?php 
+		          	<input type="hidden" name="ReceiverId"
+		          	<?php
 		          		if($userType == '1') {
-		          			echo "value=".$row->menteeId; 
+		          			echo "value=".$row->menteeId;
 		          		}
 		          		else {
 		          			echo "value=".$row->mentorId;
 		          		}
 		          	?>
 		          		>
-			        
+
 		          	<span class="glyphicon glyphicon-comment" ></span> Leave Feedback </button>
 
-		          	
+
 			        <br>
-		          	
+
 		          </form>
 
 		        </td>
@@ -98,30 +134,29 @@ function profile_connection() {
 			<?php foreach ($myFeedbacks as $feedback) { ?>
  			<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
 		    	<div class="d-flex w-100 justify-content-between">
-			      <h5 class="mb-1">Sender: <?php echo $feedback->sender; ?></h5>
-			      <!-- <small>Rating : <?php echo $feedback->Rating; ?></small> -->
+			      <h5 class="mb-1">Sender: <?php echo $feedback->SenderId; ?></h5>
+			      <small>Rating : <?php echo $feedback->Rating; ?></small>
 			    </div>
 			    <p class="mb-1">Feedback: <?php echo $feedback->Description; ?></p>
-			    <small>From:  <?php echo $feedback->receiver; ?></small>
+			    <small>From:  <?php echo $feedback->ReceiverId; ?></small>
 			    <small>Date:  <?php echo $feedback->dateCommented; ?></small>
-		  	</a>				
+
+		  	</a>
 		<?php } ?>
 		</div>
 
 		<div class="list-group">
 
-			<?php foreach ($TCFeedBacks as $tcfeedback) { ?>	
+			<?php foreach ($TCFeedBacks as $tcfeedback) { ?>
 		  	<div class="alert alert-warning">
 			  <strong>Feedback Requested</strong>
 			  <br>
 			  <?php echo $tcfeedback->Description; ?>
 			  <br>
 			  <small>Date:  <?php echo $tcfeedback->createdDate; ?></small>
-			</div>		
+			</div>
 		<?php } ?>
 		</div>
-
-
 	<?php
 	}
 	else{
