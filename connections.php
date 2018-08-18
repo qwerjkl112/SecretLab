e0d25c388c1bc0cb9f10fa712d096d96e82666a6
marina-espinoza <35943671+marina-espinoza@users.noreply.github.com>
2018-08-18 15:37:23 -0400

    Added scroll in connections page

--- connections.php
+++ connections.php
@@ -9,54 +9,63 @@ function connections_table() {
         $rows = $wpdb->get_results("SELECT users.`firstname` AS `mentorName`, users2.`firstname` AS `menteeName`, `connectionid`, `mentorId`, `menteeId`, `createdDate`, `lastConnected` FROM `connections` INNER JOIN users on connections.mentorId=users.ID INNER JOIN users users2 on connections.menteeId=users2.ID ");
 	?>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<style>
+			.wrap {
+				height: 80% !important;
+				overflow: scroll;
+			}
+		</style>
 		<div class="container">
-		  <table class="table table-striped">
-		    <thead>
-		      <tr>
-		        <th>Connection</th>
-		        <th>Last Checked In</th>
-		        <th>Delete</th>
-		        <th>Details</th>
-		      </tr>
-		    </thead>
-		    <tbody>
-		      <?php foreach ($rows as $row) { 
-		      	$cid = $row->connectionid; ?>
-		      <tr id="<?php echo $row->connectionid; ?>_row" <?php 
-		      		if(strtotime($row->lastConnected) < strtotime("-30 day"))
-		      		echo "class='warning'"; ?>>
-		        <td> 
-		          Connection Id:
-		          <a href=""><?php echo $cid; ?></a><br>
-		          Mentor Name:
-		          <a href="#"><?php echo $row->mentorName; ?></a><br>
-		          Mentee Name:
-		          <a href="#"><?php echo $row->menteeName; ?></a><br>
-		        </td>
-		        <td> 
-		          Date Connected:
-		          <?php echo $row->createdDate; ?><br>
-		          Last Checked In:
-		          <?php echo $row->lastConnected; ?><br>
-		        </td>
-		        <td> 
-		          <form action="" method="post">
-		          	<button type="submit" class="btn btn-default" name="delete_connection">
-		          	<input type="hidden" name="connectionid" <?php echo "value=".$cid;?>>
-		          	<span class="glyphicon glyphicon-remove" ></span> Deactivate Connection </button>
-		          </form>
-		        </td>
-		        <td>
-		          <form action="" method="post">
-		          	<button type="submit" class="btn btn-default" name="connection_detail">
-		          	<input type="hidden" name="connectionid" <?php echo "value=".$cid;?>>
-		          	<span class="glyphicon glyphicon-info-sign" ></span> View Comments </button>
-			      </form>
-		         </td>
-					</tr>
-			<?php } ?>
-		    </tbody>
-		  </table>
+			<h2>Connections</h2>
+			<div class="wrap">
+			  <table class="table table-striped">
+			    <thead>
+			      <tr>
+			        <th>Connection</th>
+			        <th>Last Checked In</th>
+			        <th>Delete</th>
+			        <th>Details</th>
+			      </tr>
+			    </thead>
+			    <tbody>
+			      <?php foreach ($rows as $row) {
+			      	$cid = $row->connectionid; ?>
+			      <tr id="<?php echo $row->connectionid; ?>_row" <?php
+			      		if(strtotime($row->lastConnected) < strtotime("-30 day"))
+			      		echo "class='warning'"; ?>>
+			        <td>
+			          Connection Id:
+			          <a href=""><?php echo $cid; ?></a><br>
+			          Mentor Name:
+			          <a href="#"><?php echo $row->mentorName; ?></a><br>
+			          Mentee Name:
+			          <a href="#"><?php echo $row->menteeName; ?></a><br>
+			        </td>
+			        <td>
+			          Date Connected:
+			          <?php echo $row->createdDate; ?><br>
+			          Last Checked In:
+			          <?php echo $row->lastConnected; ?><br>
+			        </td>
+			        <td>
+			          <form action="" method="post">
+			          	<button type="submit" class="btn btn-default" name="delete_connection">
+			          	<input type="hidden" name="connectionid" <?php echo "value=".$cid;?>>
+			          	<span class="glyphicon glyphicon-remove" ></span> Deactivate Connection </button>
+			          </form>
+			        </td>
+			        <td>
+			          <form action="" method="post">
+			          	<button type="submit" class="btn btn-default" name="connection_detail">
+			          	<input type="hidden" name="connectionid" <?php echo "value=".$cid;?>>
+			          	<span class="glyphicon glyphicon-info-sign" ></span> View Comments </button>
+				      </form>
+			         </td>
+						</tr>
+				<?php } ?>
+			    </tbody>
+			  </table>
+			</div>
 		</div>
 
 		<script>
@@ -81,12 +90,12 @@ function connections_table() {
 				var connection_row = e.parentElement
 				var detail_form = connection_row.querySelector('[name=connection_detail]').parentElement;
 				detail_form.style.display = "block";
-				
+
 				var comment_row = connection_row.parentElement.nextSibling
 				comment_row.remove();
 				e.remove();
 			}
-		
+
 		</script>
 
 <?php
